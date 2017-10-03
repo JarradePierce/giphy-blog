1701142133172d46ebc9c9f0e7c6457a61356c30
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-10-03 05:43:32 -0700

    re-routed failed login to root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
       redirect_to @user
     else
       flash[:notice] = "Login failed"
-      redirect_to new_session_path
+      redirect_to root_path
     end
   end
 
