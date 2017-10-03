d1866bc9d3f9d23ccf7a5f46f8319b2e54a0e07f
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-10-03 05:53:59 -0700

    changing validations for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :username, :email, :password, presence: true, uniqueness: true
+  validates :username, :email, :password, presence: true, :on => :create
+  validates :username, :email, uniqueness: true
 
   has_many :blogs
 end
