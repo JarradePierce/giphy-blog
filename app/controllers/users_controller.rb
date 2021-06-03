242f0f9fcf5f2974db9a7063ce4c8c55a2d44fff
Jarrade Pierce <x-jpierce@3572-laptop.corp.zooxlabs.com>
2021-06-03 15:45:39 -0700

    added only one user can be created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,15 +16,17 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        @defaultCategory = @user.categories.create(title: 'default', user_id: @user.id)
-        session[:id] = @user.id
-        format.html { redirect_to @user, notice: "#{@user.username}'s' new account created" }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      if User.count < 1
+      respond_to do |format|
+        if @user.save
+          @defaultCategory = @user.categories.create(title: 'default', user_id: @user.id)
+          session[:id] = @user.id
+          format.html { redirect_to @user, notice: "#{@user.username}'s' new account created" }
+          format.json { render :show, status: :created, location: @user }
+        else
+          format.html { render :new }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
