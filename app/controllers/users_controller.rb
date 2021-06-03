9b0890e7f669ef9ec02a9dcc88e4d9ffa23d5b4e
Jarrade Pierce <x-jpierce@3572-laptop.corp.zooxlabs.com>
2021-06-03 15:51:14 -0700

    added validation check to new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
 
-      if User.count < 1
+      if @user.valid? && User.count < 1
       respond_to do |format|
         if @user.save
           @defaultCategory = @user.categories.create(title: 'default', user_id: @user.id)
