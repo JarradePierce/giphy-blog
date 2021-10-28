6755182091026817e05a67f32cf9eb014060c959
Jarrade Pierce <x-jpierce@3572-laptop.corp.zooxlabs.com>
2021-10-28 15:31:39 -0700

    changed completed to bool

--- db/migrate/20211028192017_add_completed_to_blogs.rb
+++ db/migrate/20211028192017_add_completed_to_blogs.rb
@@ -1,5 +1,5 @@
 class AddCompletedToBlogs < ActiveRecord::Migration[5.1]
   def change
-    add_column :blogs, :completed, :boolean, :default => 0
+    add_column :blogs, :completed, :boolean, :default => false
   end
 end
